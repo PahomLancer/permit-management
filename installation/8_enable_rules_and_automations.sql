2fae23b9e7a24de7e8c08fd9a0b94cb85ba0742e
Didenko Andrey <mozggg1@gmail.com>
2022-10-23 23:52:06 +0700

    Adding object reference rebuild and enabling rules and automations

---
+++ installation/8_enable_rules_and_automations.sql
@@ -0,0 +1,12 @@
+begin
+    update rule
+       set is_enabled = 1
+     where rule in (
+     'PRM_PERMIT_COUNTDOWN nightly', 'PRM_PERMIT_COUNTDOWN wtu', 'PRM_PERMIT_EXPECTED', 'PRM_PERMIT_EXPIRED nightly', 
+     'PRM_PERMIT_EXPIRED cfu', 'PRM_PERMIT_EXPIRES_BEFORE_CX_S cfu', 'PRM_PERMIT_EXPIRED wtu', 'PRM_PERMIT_COUNTDOWN cfu', 
+     'PRM_PERMIT_EXPIRES_BEFORE_CX_S wtu', 'PRM_PERMIT_AGE nightly', 'New Permits', 'PRM_PERMIT_AGE cfu'
+    );
+    update autom
+       set enabled = 1
+     where autom_name in ('Permit Status Change Date', 'Permit Approved Autofill', 'PRM_PERMIT_STATUS');
+end;
