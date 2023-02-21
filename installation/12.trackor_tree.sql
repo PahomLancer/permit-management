03ba3e33fd4c078fb0961fe5e1a5cb2be731f324
Didenko Andrey <mozggg1@gmail.com>
2023-02-21 15:08:49 +0700

    Updating v_table names, package name and script to match standards

--- installation/12.trackor_tree.sql
+++ installation/12.trackor_tree.sql
@@ -1,5 +1,6 @@
 declare
     c_rel_cardinality_one_many constant number := 2;
+    v_program_id number;
     v_jurisdiction_ttid xitor_type.xitor_type_id%type;
     v_permits_ttid xitor_type.xitor_type_id%type;
     v_project_ttid xitor_type.xitor_type_id%type;
@@ -9,9 +10,10 @@ declare
     v_rtid4 relation_type.relation_type_id%type;
     v_rtid5 relation_type.relation_type_id%type;
 begin
-    select xitor_type_id into v_jurisdiction_ttid from xitor_type where xitor_type = 'Jurisdiction';
-    select xitor_type_id into v_permits_ttid from xitor_type where xitor_type = 'Permits';
-    select xitor_type_id into v_project_ttid from xitor_type where xitor_type = 'Project'; /*General trackor type*/
+    v_program_id := pkg_sec.get_pid();
+    select xitor_type_id into v_jurisdiction_ttid from xitor_type where xitor_type = 'Jurisdiction' and program_id = v_program_id;
+    select xitor_type_id into v_permits_ttid from xitor_type where xitor_type = 'Permits' and program_id = v_program_id;
+    select xitor_type_id into v_project_ttid from xitor_type where xitor_type = 'Project' and program_id = v_program_id; /*General trackor type*/
     /*NEW_RELATION_TYPE(PID IN NUMBER, CID IN NUMBER, CARDINALITY IN NUMBER, CHILDREQPARENT IN NUMBER, ONDELETECASCADE IN NUMBER, UNIQUEBY IN NUMBER DEFAULT NULL)*/
     v_rtid1 := pkg_relation.new_relation_type(null, v_project_ttid, c_rel_cardinality_one_many, 0, 0);
     v_rtid2 := pkg_relation.new_relation_type(null, v_jurisdiction_ttid, c_rel_cardinality_one_many, 0, 0);
